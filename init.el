9b267bbcfc4f3e005f61d4b7b8dc874569489557
Samuel Bourasseau <samuel.bourasseau@allegorithmic.com>
2018-01-05 17:04:20 +0100

    add cc-mode 5.33 to support modern C++ features

--- init.el
+++ init.el
@@ -1,3 +1,19 @@
+
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+(defconst modern-cc-mode-path (file-truename (concat (getenv "HOME") "/.emacs.d/site-lisp/cc-mode-5.33")))
+(byte-recompile-directory modern-cc-mode-path)
+(add-to-list 'load-path modern-cc-mode-path)
+
+
 (defun previous-blank-line ()
   "Moves to the previous empty line"
   (interactive)
@@ -22,6 +38,8 @@
 (global-set-key (kbd "M-n") 'next-blank-line)
 (global-set-key (kbd "C-'") 'whitespace-mode)
 
+(defalias 'list-buffers 'ibuffer)
+
 (setq c-echo-syntactic-information-p t)
 (c-add-style "yolosquad"
 	     '((c-basic-offset . 4)
@@ -29,6 +47,8 @@
 	       (c-offsets-alist
 		(innamespace . 0)
 		(substatement-open . 0)
+		(comment-intro . 0)
+		(inlambda . 0)
 		)))
 
 (setq c-default-style
@@ -38,19 +58,26 @@
 	(c++-mode . "yolosquad")
 	(other . "gnu")))
 
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+;;(add-hook 'c-mode-hook (lambda () (auto-fill-mode 1)))
+
 (setq make-backup-files nil)
 
 (add-to-list 'default-frame-alist '(font . "Liberation Mono-10.5"))
 (set-face-attribute 'default t :font "Liberation Mono-10.5")
 (set-face-attribute 'font-lock-builtin-face nil :foreground "#DAB98F")
 (set-face-attribute 'font-lock-comment-face nil :foreground "gray50")
-(set-face-attribute 'font-lock-constant-face nil :foreground "olive drab")
+(set-face-attribute 'font-lock-constant-face nil :foreground "OliveDrab")
 (set-face-attribute 'font-lock-doc-face nil :foreground "gray50")
-(set-face-attribute 'font-lock-function-name-face nil :foreground "CadetBlue")
+;;(set-face-attribute 'font-lock-function-name-face nil :foreground "CadetBlue")
+(set-face-attribute 'font-lock-function-name-face nil :foreground "burlywood3")
 (set-face-attribute 'font-lock-keyword-face nil :foreground "DarkGoldenrod3")
-(set-face-attribute 'font-lock-string-face nil :foreground "OliveDrab")
+(set-face-attribute 'font-lock-string-face nil :foreground "Sienna")
 (set-face-attribute 'font-lock-type-face nil :foreground "SeaGreen4")
-(set-face-attribute 'font-lock-variable-name-face nil :foreground "DodgerBlue3")
+;;(set-face-attribute 'font-lock-type-face nil :foreground "burlywood3")
+;;(set-face-attribute 'font-lock-variable-name-face nil :foreground "DodgerBlue3")
+(set-face-attribute 'font-lock-variable-name-face nil :foreground "burlywood3")
 (set-face-attribute 'escape-glyph nil :foreground "coral" :background "firebrick")
 (set-foreground-color "burlywood3")
 (set-background-color "#161616")
@@ -60,31 +87,84 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (show-paren-mode 1)
-(desktop-save-mode 1)
+;;(desktop-save-mode 1)
 (setq desktop-save 'ask-if-new)
 (setq desktop-dirname "~/")
 (setq desktop-base-file-name ".emacs.desktop")
-(add-hook 'after-change-major-mode-hook
-	  (lambda () (setq desktop-save-buffer t)))
-;(global-hl-line-mode 1)
+(setq inhibit-startup-screen 1)
+(if (eq desktop-save-mode nil)
+    (progn
+      (switch-to-buffer "*scratch*")
+      )
+  )
+;;(add-hook 'after-change-major-mode-hook
+;;	  (lambda () (setq desktop-save-buffer t)))
+;;(global-hl-line-mode 1)
+
 
 
 (if (equal system-type 'windows-nt)
     (progn
-      (defconst VS2015_MsBuild_Path (getenv "VS140COMNTOOLS"))
-      (defconst VS2017_MsBuild_Path (getenv "VS160COMNTOOLS"))
-      (defconst MsBuild_FileName "VsMSBuildCmd.bat")
+      (package-install 'msvc)
+      (add-to-list 'load-path (expand-file-name "msvc/" "~/.emacs.d"))
+      (require 'msvc)
+      (setq w32-pipe-read-delay 0)
+      (when (msvc-initialize)
+	(msvc-flags-load-db :parsing-buffer-delete-p t)
+	(add-hook 'c-mode-common-hook 'msvc-mode-on t)
+	)
+      (add-hook 'msvc-mode-hook
+		(lambda ()
+		  (local-unset-key (kbd "<C-f5>"))
+		  (local-set-key (kbd "<C-f5>") 'msvc-mode-feature-build-project)
+		  ))
+      (defun msvc-load-solution (path version &optional target platform)
+	(interactive)
+	(if (eq target nil)
+	    (setq target "Release")
+	  )
+	(if (eq platform nil)
+	    (setq platform "x64")
+	  )
+	(msvc-activate-projects-after-parse :solution-file path
+					    :platform platform
+					    :configuration target
+					    :version version)
+	)
+      (defun msvc-load-project (path version &optional target platform)
+	(interactive)
+	(if (eq target nil)
+	    (setq target "Release")
+	  )
+	(if (eq platform nil)
+	    (setq platform "x64")
+	  )
+	(msvc-activate-projects-after-parse :project-file path
+					    :platform platform
+					    :configuration target
+					    :version version)
+	)
+      (defun msvc-unload-all ()
+	(interactive)
+	(kill-matching-buffers "\*MSVC.*\*")
+	)
       (if (equal (getenv "HOME") nil)
 	  (print "HOME variable is undefined")
 	)
-;      (if (equal (require 'cygwin-mount nil t) nil)
-;	  (print "cygwin-mount missing from load-path folder")
-;	)
-;      (cygwin-mount-activate)
-;      (setq explicit-shell-file-name "bash")
-;      (setq shell-file-name explicit-shell-file-name)
-;      (add-hook 'comint-output-filter-functions 'shell-strip-ctrl-m nil t)
-;      (add-hook 'comint-output-filter-functions 'comint-watch-for-password-prompt nil t)
-      (print "Setup windows environment succesfully")
+      (message "Setup windows environment succesfully")
       )
   )
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (msvc))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
